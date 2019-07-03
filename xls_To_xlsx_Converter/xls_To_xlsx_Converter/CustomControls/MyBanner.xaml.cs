26b10439bc43bf561fe60e6c3d3b14e3e5890cfe
john omelko <somethingTacos@hotmail.com>
2019-07-03 17:35:34 -0400

    I've got a potentially better idea for the banner. Have to test it though...

---
+++ xls_To_xlsx_Converter/xls_To_xlsx_Converter/CustomControls/MyBanner.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xls_To_xlsx_Converter.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for MyBanner.xaml
+    /// </summary>
+    public partial class MyBanner : UserControl
+    {
+        public MyBanner()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(MyBanner), new PropertyMetadata(false));
+
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+    }
+}
