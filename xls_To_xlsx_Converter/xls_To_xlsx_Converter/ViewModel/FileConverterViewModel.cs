d5693c694de51046ee05551cf386c51ce6df2081
john omelko <somethingTacos@hotmail.com>
2019-07-18 13:50:13 -0400

    blah

--- xls_To_xlsx_Converter/xls_To_xlsx_Converter/ViewModel/FileConverterViewModel.cs
+++ xls_To_xlsx_Converter/xls_To_xlsx_Converter/ViewModel/FileConverterViewModel.cs
@@ -322,14 +322,21 @@ namespace xls_To_xlsx_Converter.ViewModel
         }
         public string ConvertXLS_XLSX(FileInfo file)
         {
-            var app = new Microsoft.Office.Interop.Excel.Application();
-            var xlsFile = file.FullName;
-            var wb = app.Workbooks.Open(xlsFile);
-            var xlsxFile = xlsFile + "x";
-            wb.SaveAs(Filename: xlsxFile, FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook);
-            wb.Close();
-            app.Quit();
-            return xlsxFile;
+            try
+            {
+                var app = new Microsoft.Office.Interop.Excel.Application();
+                var xlsFile = file.FullName;
+                var wb = app.Workbooks.Open(xlsFile);
+                var xlsxFile = xlsFile + "x";
+                wb.SaveAs(Filename: xlsxFile, FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook);
+                wb.Close();
+                app.Quit();
+                return xlsxFile;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         public void onCancelSearchCommand(object parameter)
